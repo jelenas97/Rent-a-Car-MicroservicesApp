3fb26b4dff9b50de15902c73008be6cec5863cee
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-03 19:05:57 +0200

    moving rent to location, fixing docker-compose ...

--- rent/src/main/java/com/rent/RentApplication.java
+++ rent/src/main/java/com/rent/RentApplication.java
@@ -2,8 +2,10 @@ package com.rent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RentApplication {
     public static void main(String[] args){
         SpringApplication.run(RentApplication.class,args);
