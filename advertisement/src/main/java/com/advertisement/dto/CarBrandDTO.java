2bc090467773bfdad5159e1e032ac29db769599e
jelenas97 <jekibp@gmail.com>
2020-06-05 00:54:43 +0200

    Primitive Types DTOS added

---
+++ advertisement/src/main/java/com/advertisement/dto/CarBrandDTO.java
@@ -0,0 +1,15 @@
+package com.advertisement.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarBrandDTO {
+    private Long id;
+    private String name;
+
+}
---
+++ advertisement/src/main/java/com/advertisement/dto/CarClassDTO.java
@@ -0,0 +1,13 @@
+package com.advertisement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarClassDTO {
+    private Long id;
+    private String name;
+}
