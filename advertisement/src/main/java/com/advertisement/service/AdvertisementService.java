f03355879f27504aa2965f1d953d20781a652618
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-05 15:53:51 +0200

    added search and findAll methods

--- advertisement/src/main/java/com/advertisement/service/AdvertisementService.java
+++ advertisement/src/main/java/com/advertisement/service/AdvertisementService.java
@@ -1,6 +1,13 @@
 package com.advertisement.service;
 
+import com.advertisement.dto.SearchDTO;
+import com.advertisement.model.Advertisement;
+
+import java.util.List;
+
 public interface AdvertisementService {
 
+    List<Advertisement> findAll();
+    List<Advertisement> search(SearchDTO dto);
 
 }
