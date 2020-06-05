fd758cc688816a969a53f8c6c4571428ae56a4af
jelenas97 <jekibp@gmail.com>
2020-06-05 19:08:56 +0200

    Changed annotation on class

--- advertisement/src/main/java/com/advertisement/controller/CodeBookController.java
+++ advertisement/src/main/java/com/advertisement/controller/CodeBookController.java
@@ -10,17 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.PermitAll;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@Controller
+@RestController
 @RequestMapping(value = "codebook")
 @CrossOrigin("http://localhost:4200")
 public class CodeBookController {
