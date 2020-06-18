c659989d809f9ade38270c126fb56ad1f277676a
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-19 01:33:00 +0200

    Update CommentController.java

--- statistics/src/main/java/com/statistics/controller/CommentController.java
+++ statistics/src/main/java/com/statistics/controller/CommentController.java
@@ -1,12 +1,61 @@
 package com.statistics.controller;
 
+import com.statistics.dto.CommentDTO;
+import com.statistics.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/comment", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin("http://localhost:4200")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    //@PreAuthorize("hasAuthority('ROLE_CLIENT')")
+    public ResponseEntity<?> addComment(@RequestBody CommentDTO dto) {
+
+        try {
+            long id = this.commentService.addComment(dto);
+            return new ResponseEntity(id, HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping(value="/owner", produces = "application/json", consumes = "application/json")
+    //@PreAuthorize("hasAnyAuthority('ROLE_CLIENT','ROLE_AGENT')")
+    public ResponseEntity<?> addCommentOwner(@RequestBody CommentDTO dto) {
+
+        try {
+            long id = this.commentService.addCommentOwner(dto);
+            return new ResponseEntity(id, HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping(value="/{id}", produces="application/json")
+    //@PreAuthorize("hasAnyAuthority('ROLE_CLIENT','ROLE_AGENT')")
+    public ResponseEntity<?> getProcessedAdvertisementComments(@PathVariable Long id){
+
+        try {
+            List<CommentDTO> comments = this.commentService.findProcessedAdvertisementComments(id);
+
+            return new ResponseEntity(comments, HttpStatus.OK);
+
+        }catch(NullPointerException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error while loading comments");
+        }
+    }
 }
