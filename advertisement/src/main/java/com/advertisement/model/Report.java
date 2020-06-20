b6a01d15bbe4910b7593fae769cf3399cc6316f5
jelenas97 <jekibp@gmail.com>
2020-06-21 01:02:10 +0200

    Added annotation in model class to support soap

--- advertisement/src/main/java/com/advertisement/model/Report.java
+++ advertisement/src/main/java/com/advertisement/model/Report.java
@@ -1,26 +1,38 @@
 package com.advertisement.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 
 @Entity
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name="Report", namespace = "http://localhost:8084/microservices/report")
+@XmlRootElement(name = "reportClass")
 public class Report {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @XmlElement
     private Long id;
 
     @Column
+    @XmlElement
     private Integer kilometers;
 
     @Column
+    @XmlElement
     private String description;
 
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     @JoinColumn(name = "term_id", referencedColumnName = "id")
+    @XmlElement
     private Term term;
 
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     @JoinColumn(name = "advertisement_id", referencedColumnName = "id")
+    @XmlElement
     private Advertisement advertisement;
 
     public Report() {
@@ -66,4 +78,14 @@ public class Report {
         this.advertisement = advertisement;
     }
 
+    @Override
+    public String toString() {
+        return "Report{" +
+                "id=" + id +
+                ", kilometers=" + kilometers +
+                ", description='" + description + '\'' +
+                ", term=" + term +
+                ", advertisement=" + advertisement +
+                '}';
+    }
 }
