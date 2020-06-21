11d38e77793c2d3abbefb03766309bb24c8e185b
jovica.josipovic97@gmail.com <jovica.josipovic97@gmail.com>
2020-06-21 16:15:31 +0200

    Data sql

--- statistics/src/main/resources/data.sql
+++ statistics/src/main/resources/data.sql
@@ -2,3 +2,5 @@ insert into comment(content, date, advertisement_id, rent_request_id, user_id, s
 
 insert into rate(value, advertisement_id, client_id, rent_request_id)
 values(9, 3, 1, 3);
+insert into comment(content, date, advertisement_id, rent_request_id, user_id, status)
+values ('Mikroservisni komentar!!!!!!', '2020-06-13', 3, 3, 1, 'APPROVED');
