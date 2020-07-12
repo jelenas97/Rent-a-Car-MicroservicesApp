ec29aa8c5fe430aa8ea4a8c40e23a553cfe29b8d
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-07-12 02:02:06 +0200

    extraPayDTOs

---
+++ advertisement/src/main/java/com/advertisement/dto/ExtraPayDTO.java
@@ -0,0 +1,18 @@
+package com.advertisement.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ExtraPayDTO {
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private long advertisementId;
+    private long extraPayPrice;
+}
